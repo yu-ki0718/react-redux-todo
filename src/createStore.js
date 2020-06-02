da5498ea37c3891556de7a564eb08fb374481cb5
岸波　雄樹 <you_work_24@icloud.com>
2020-06-02 13:50:17 +0900

    created redux-store

---
+++ src/createStore.js
@@ -0,0 +1,17 @@
+import {
+  createStore as reduxCreateStore,
+  applyMiddleware,
+  combineReducers,
+} from "redux";
+import logger from "redux-logger";
+import { todoReducer } from "./reducers/Todo";
+
+export default function createStore() {
+  const store = reduxCreateStore(
+    combineReducers({
+      todo: todoReducer,
+    }),
+    applyMiddleware(logger)
+  );
+  return store;
+}
